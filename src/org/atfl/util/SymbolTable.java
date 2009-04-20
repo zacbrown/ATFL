65de99bedb147bd1868043517e0929e9e3658ecf
Zac Brown <zac@zacbrown.org>
2009-04-20 13:56:37 -0400

    Re-engineer SymbolTable to better match current needs.

--- src/org/atfl/util/SymbolTable.java
+++ src/org/atfl/util/SymbolTable.java
@@ -5,17 +5,26 @@ import org.atfl.exception.SymbolException;
 import org.atfl.runtime.ControlNode;
 
 public class SymbolTable {
-    private HashMap<ControlNode, ControlNode> symbols;
-    private ControlNode associatedBlock;
-    private SymbolTable parent;
+    private HashMap<String, ControlNode> symbols;
+    private ControlNode associatedBlock = null;
+    private SymbolTable parent = null;
+
+    public SymbolTable() {
+        symbols = new HashMap<String, ControlNode>();
+    }
+
+    public SymbolTable(SymbolTable parent) {
+        this.parent = parent;
+        symbols = new HashMap<String, ControlNode>();
+    }
 
     public SymbolTable(ControlNode associatedBlock, SymbolTable parent) {
         this.associatedBlock = associatedBlock;
         this.parent = parent;
-        symbols = new HashMap<ControlNode, ControlNode>();
+        symbols = new HashMap<String, ControlNode>();
     }
 
-    public void add(ControlNode n, ControlNode value) throws SymbolException {
+    public void add(String n, ControlNode value) throws SymbolException {
         if (symbols.containsKey(n)) {
             throw new SymbolException("Cannot redefine symbol '" + n.toString() + "'");
         }
@@ -25,5 +34,8 @@ public class SymbolTable {
 
     public ControlNode getAssociatedBlock() { return associatedBlock; }
     public SymbolTable getParent() { return parent; }
-    public ControlNode get(ControlNode n) { return symbols.get(n); }
+    public ControlNode get(String n) { return symbols.get(n); }
+
+    @Override
+    public String toString() { return symbols.toString(); }
 }
