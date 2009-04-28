1dbe3fb1ad932cbf656c7fb89052f42f227487a0
Zac Brown <zac@zacbrown.org>
2009-04-27 21:55:52 -0400

    Add 'set' keyword for assigning expressions to names. This works for both assigning names to anonymous functions as well as to values or lists themselves.

--- src/org/atfl/runtime/translator/ATFLTranslator.java
+++ src/org/atfl/runtime/translator/ATFLTranslator.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.Stack;
 import java.util.Vector;
 import java.util.regex.Pattern;
+import org.atfl.exception.FunctionCallException;
 import org.atfl.exception.SymbolException;
 import org.atfl.exception.TranslatorException;
 import org.atfl.runtime.ControlNode;
@@ -28,7 +29,7 @@ public class ATFLTranslator {
     public ControlNode getControl() { return control; }
     public Stack getEnv() { return env; }
 
-    public void translateTop() throws TranslatorException, SymbolException {
+    public void translateTop() throws TranslatorException, SymbolException, FunctionCallException {
         if (AST.getTag().equals(ParserNodeTag.LIST) == false) {
             throw new TranslatorException("Expected TOP node, got '" +
                     AST.getTag() + "'");
@@ -36,10 +37,16 @@ public class ATFLTranslator {
 
         ControlNode code = new ControlNode(ControlNodeTag.LIST, new Vector<ControlNode>());
         //code.addSubNode(new ControlNode(ControlNodeTag.INSTR, OpCode.AP));
-        code.addSubNode(new ControlNode(ControlNodeTag.INSTR, OpCode.STOP));
         Vector<ParserNode> nodes = AST.getSubNodes();
         env.push(new SymbolTable());
         int size = nodes.size();
+
+        for (int i = 0; i < size; i++) {
+            control = translate((ParserNode)nodes.get(i), env, code);
+        }
+        code = new ControlNode(ControlNodeTag.LIST, new Vector<ControlNode>());
+        code.addSubNode(new ControlNode(ControlNodeTag.INSTR, OpCode.STOP));
+
         for (int i = size - 1; i >= 0; i--) {
             control = translate((ParserNode)nodes.get(i), env, code);
         }
@@ -77,7 +84,7 @@ public class ATFLTranslator {
         return retControl;
     }
 
-    public ControlNode translate(ParserNode expr, Stack env, ControlNode code) throws TranslatorException, SymbolException {
+    public ControlNode translate(ParserNode expr, Stack env, ControlNode code) throws TranslatorException, SymbolException, FunctionCallException {
         if (expr.getTag().equals(ParserNodeTag.ATOM)) {
             ControlNode n = translateAtom(expr, env, false);
             ListUtils.cons(code, n);
@@ -103,6 +110,14 @@ public class ATFLTranslator {
                 ListUtils.cons(code, new ControlNode(ControlNodeTag.INSTR, OpCode.AP));
             }
             return translate((ParserNode)ListUtils.car(expr), env, code);
+        }
+        else if (((ParserNode)ListUtils.car(expr)).getValue().equals("set")) {
+            ControlNode body = new ControlNode(ControlNodeTag.LIST, new Vector<ControlNode>());
+            ParserNode name = (ParserNode)ListUtils.cadr(expr);
+            ParserNode setExpr = (ParserNode)ListUtils.caddr(expr);
+            SymbolTable curEnv = (SymbolTable)env.peek();
+            curEnv.add((String)name.getValue(), translate(setExpr, env, body));
+            Collections.reverse(body.getSubNodes());
         }
         else if (((ParserNode)ListUtils.car(expr)).getValue().equals("print")) {
             ListUtils.cons(code, new ControlNode(ControlNodeTag.INSTR, OpCode.PRINT));
@@ -190,25 +205,79 @@ public class ATFLTranslator {
 
             Vector<ParserNode> args = ((ParserNode)ListUtils.cadr(expr)).getSubNodes();
             int argSize = args.size();
-
-            Vector<ControlNode> argVals = new Vector<ControlNode>();
-            for (int i = 0; i < argSize; i++) {
-                argVals.add(0, (ControlNode) env.pop());
-            }
-
-            for (int i = 0; i < argSize; i++) {
-                ParserNode n = args.get(i);
-                newEnv.add((String) n.getValue(), argVals.get(i));
-            }
-            env.push(newEnv);
-
+            ControlNode ldf = new ControlNode(ControlNodeTag.INSTR, OpCode.LDF);
             ControlNode funControl = new ControlNode(ControlNodeTag.LIST, new Vector<ControlNode>());
             funControl.addSubNode(new ControlNode(ControlNodeTag.INSTR, OpCode.RTN));
-            ControlNode body = translate((ParserNode)ListUtils.caddr(expr), env, funControl);
+            ControlNode body = null;
+            if (env.peek() instanceof SymbolTable) {
+                String[] funcArgs = new String[argSize];
+                SymbolTable tmpEnv = new SymbolTable();
+                for (int i = 0; i < argSize; i++) {
+                    ParserNode n = args.get(i);
+                    funcArgs[i] = (String)n.getValue();
+                    tmpEnv.add((String)n.getValue(), new ControlNode(ControlNodeTag.NIL));
+                }
+                ldf.setFuncArgs(funcArgs);
+                env.push(tmpEnv);
+                body = translate((ParserNode)ListUtils.caddr(expr), env, funControl);
+                env.pop();
+            }
+            else {
+                Vector<ControlNode> argVals = new Vector<ControlNode>();
+                for (int i = 0; i < argSize; i++) {
+                    argVals.add(0, (ControlNode) env.pop());
+                }
+
+                for (int i = 0; i < argSize; i++) {
+                    ParserNode n = args.get(i);
+                    newEnv.add((String) n.getValue(), argVals.get(i));
+                }
+                env.push(newEnv);
+                ldf.setEnv(env);
+                body = translate((ParserNode)ListUtils.caddr(expr), env, funControl);
+            }
+
             Collections.reverse(body.getSubNodes());
             ListUtils.cons(code, body);
-            ListUtils.cons(code, new ControlNode(ControlNodeTag.INSTR, OpCode.LDF));
+            ListUtils.cons(code, ldf);
             return code;
+        }
+        else {
+            String name = (String)((ParserNode)ListUtils.car(expr)).getValue();
+            SymbolTable curEnv = (SymbolTable)env.peek();
+            ControlNode funcBody = curEnv.get(name);
+            ControlNode tmpCode = new ControlNode(ControlNodeTag.LIST, new Vector<ControlNode>());
+            if (funcBody != null) {
+                Vector<ControlNode> nodes = funcBody.getSubNodes();
+                for (int i = nodes.size() - 1; i >= 0; i--) {
+                    ControlNode node = nodes.get(i);
+                    ListUtils.cons(tmpCode, node);
+                }
+                Vector<ParserNode> argVals = ((Vector)ListUtils.cdr(expr));
+                Vector<ControlNode> tmpCodeNodes = tmpCode.getSubNodes();
+                String[] funcArgs = (tmpCodeNodes.get(tmpCodeNodes.size()-1)).getFuncArgs();
+                int argValSize = argVals.size();
+                SymbolTable newEnv = new SymbolTable((SymbolTable) env.peek());
+
+                if (funcArgs.length != argValSize) {
+                    throw new FunctionCallException("Expected " + funcArgs.length +
+                            " arguments, but got " + argValSize);
+                }
+                for (int i = 0; i < argValSize; i++) {
+                    ParserNode arg = argVals.get(i);
+                    if (arg.getTag().equals(ParserNodeTag.ATOM)) {
+                        newEnv.add(funcArgs[i], translateAtom(arg, env, true));
+                    }
+                    else {
+                        newEnv.add(funcArgs[i], translateList(arg, env));
+                    }
+                }
+                env.push(newEnv);
+                ListUtils.cons(code, new ControlNode(ControlNodeTag.INSTR, OpCode.AP));
+                ListUtils.cons(code, new ControlNode(ControlNodeTag.SYMBOL, name));
+                ListUtils.cons(code, new ControlNode(ControlNodeTag.INSTR, OpCode.LDFS));
+
+            }
         }
         return code;
     }
